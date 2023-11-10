86c99812ae41a1942e5ecb1287c694667be488a0
Jordan Cox <jordancox747@outlook.com>
2023-11-10 04:54:23 -0700

    4 tests passing

--- test/book.js
+++ test/book.js
@@ -8,6 +8,10 @@ const should = chai.should();
 
 chai.use(chaiHttp); // use allows for using middleware
 
+// beforeEach(() => { // runs before each test
+//   Book.removeAll();
+// });
+
 describe('Books', () => {
   describe('/GET book', () => {
     it('should GET all the books', (done) => {
@@ -21,5 +25,80 @@ describe('Books', () => {
           done();
         });
     });
-  })
+  });
+  describe('/POST book', () => {
+    it('should POST a book', (done) => {
+      // arrange
+      const book = {
+        title: 'The Bible',
+        author: 'God',
+        year: -2000,
+        pages: 1200
+      };
+
+      // act
+      chai
+        .request(server)
+        .post('/book')
+        .send(book)
+
+      // assert
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.be.an('object');
+          res.body.should.have.property('title');
+          res.body.should.have.property('author');
+          res.body.should.have.property('pages');
+          res.body.should.have.property('year');
+          done();
+        });
+    });
+    it('should not POST a book without pages field', (done) => {
+      // arrange
+      const book = {
+        title: 'The Bible',
+        author: 'God',
+        year: -2000,
+      };
+
+      // act
+      chai
+        .request(server)
+        .post('/book')
+        .send(book)
+
+      // assert
+        .end((err, res) => {
+          res.should.have.status(400);
+          done();
+        });
+    });
+  });
+  describe('/GET book/:id', () => {
+    it('should GET a book by a given id', (done) => {
+      // arrange
+      const book = {
+        title: 'The Fellowship of the Ring',
+        author: 'J.R.R. Tolkien',
+        year: 1954,
+        pages: 423
+      };
+
+      chai 
+        .request(server)
+        .post('/book')
+        .send(book);
+      
+      // act
+      chai
+        .request(server)
+        .get('/book/1')
+        // assert
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.have.property('id').equal(1);
+          done();
+        });
+    });
+  });
 });
